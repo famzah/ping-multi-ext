9ebc7e609e9322120c756ec30cf9710dfb84a7d3
Ivan Zahariev <famzah@famzah.net>
2020-10-14 22:09:41 +0300

    Refactor into a function

--- ping_geo/proc.py
+++ ping_geo/proc.py
@@ -48,36 +48,41 @@ def start_all_processes(hosts_data):
         fd_lookup[pipe_r] = hostname
     return fd_lookup
 
+def handle_pipes(hosts_data, fd_lookup, fdlist, exited_hosts):
+    (fds_read_ready, _, _) = select.select(fdlist, [], [], 0.05)
+    for fd in fds_read_ready:
+        s = os.read(fd, 1024 * 1024)
+        if not len(s): # EOF
+            s = '\nCommand terminated.\n'
+            fdlist.remove(fd)
+            exited_hosts.append(fd_lookup[fd])
+        else:
+            s = s.decode('ascii', 'replace')
+
+        all_s_parts = s.split('\n')
+
+        s_ends_newline = s.endswith('\n')
+        if s_ends_newline:
+            all_s_parts.pop() # remove this empty line which shows that "s" ends in "\n"
+
+        data = hosts_data[fd_lookup[fd]]
+        with data['lock']:
+            for idx, part in enumerate(all_s_parts): # all we have left here is real data
+                if idx != len(all_s_parts) - 1: # not last element
+                    newline = True # all non-last elements ended in "\n" and were split
+                else: # last element
+                    newline = s_ends_newline
+
+                if not data['raw_complete']: # last line didn't end with "\n"
+                    data['raw'][-1] += part # append to the existing line
+                else: # last line was ended with "\n"
+                    data['raw'].append(part) # start a new line
+
+                data['raw_complete'] = newline
+
 def update_hosts_data(hosts_data, fd_lookup):
     fdlist = list(fd_lookup.keys())
+    exited_hosts = []
 
     while True:
-        (fds_read_ready, _, _) = select.select(fdlist, [], [], 0.05)
-        for fd in fds_read_ready:
-            s = os.read(fd, 1024 * 1024)
-            if not len(s): # EOF
-                fdlist.remove(fd)
-                s = '\nCommand terminated.\n'
-            else:
-                s = s.decode('ascii', 'replace')
-
-            all_s_parts = s.split('\n')
-
-            s_ends_newline = s.endswith('\n')
-            if s_ends_newline:
-                all_s_parts.pop() # remove this empty line which shows that "s" ends in "\n"
-
-            data = hosts_data[fd_lookup[fd]]
-            with data['lock']:
-                for idx, part in enumerate(all_s_parts): # all we have left here is real data
-                    if idx != len(all_s_parts) - 1: # not last element
-                        newline = True # all non-last elements ended in "\n" and were split
-                    else: # last element
-                        newline = s_ends_newline
-
-                    if not data['raw_complete']: # last line didn't end with "\n"
-                        data['raw'][-1] += part # append to the existing line
-                    else: # last line was ended with "\n"
-                        data['raw'].append(part) # start a new line
-
-                    data['raw_complete'] = newline
+        handle_pipes(hosts_data, fd_lookup, fdlist, exited_hosts)
