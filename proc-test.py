77ea80093b704c931578508fc7f96f774c627966
Ivan Zahariev <famzah@famzah.net>
2020-10-16 09:33:57 +0300

    Initial commit

---
+++ proc-test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import ping_geo.proc
+import threading
+
+hosts_data = {
+    'aws.amazon.com': {
+        'proc': {
+            'cmdline': 'ssh root@dd ping -n aws.amazon.com',
+        },
+        'parsed': [''],
+        'raw': [''],
+        'raw_complete': False,
+        'lock': threading.Lock(),
+    },
+    'google.com': {
+        'proc': {
+            'cmdline': 'ssh root@dd ping -n google.com',
+        },
+        'parsed': [''],
+        'raw': [''],
+        'raw_complete': False,
+        'lock': threading.Lock(),
+    },
+    'google.com-typo': {
+        'proc': {
+            'cmdline': 'zssh root@dd ping -n google.comZZZ',
+        },
+        'parsed': [''],
+        'raw': [''],
+        'raw_complete': False,
+        'lock': threading.Lock(),
+    },
+}
+
+workflow = ping_geo.proc.Workflow(hosts_data)
+workflow.debug = True
+workflow.start_all_processes()
+while True:
+    workflow.update_hosts_data(0.05)
